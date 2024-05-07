1a01f0976ba077ae844d6a27c1ecea7a1c896f01
luis guilherme <tete42449@gmail.com>
2024-05-07 01:14:59 -0300

    inicio

---
+++ aumento salarial.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h> 
+#include <string.h>
+
+
+float salario,percentual,aumento,novoSalario;
+int main(){
+    setlocale(LC_ALL,"Portuguese_Breazil");
+    printf("insira o seu salário atual:\n");
+    scanf("%f", salario);
+
+
+    return 0;
+}
