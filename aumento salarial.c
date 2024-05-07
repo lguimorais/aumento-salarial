ff1c1254f21195e52b57445aa696513599b13ca8
luis guilherme <tete42449@gmail.com>
2024-05-07 01:19:42 -0300

    codigo finalizado

--- aumento salarial.c
+++ aumento salarial.c
@@ -1,15 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <locale.h> 
+#include <locale.h>
 #include <string.h>
 
-
-float salario,percentual,aumento,novoSalario;
-int main(){
-    setlocale(LC_ALL,"Portuguese_Breazil");
-    printf("insira o seu salário atual:\n");
-    scanf("%f", salario);
-
+float salario, percentual, aumento, novoSalario;
+int main()
+{
+    setlocale(LC_ALL, "Portuguese_Breazil");
+    printf("insira o seu salario atual:\n");
+    scanf("%f", &salario);
+    printf("incira o percentual de aumento:\n");
+    scanf("%f", &percentual);
+    aumento=salario* percentual/100;
+    novoSalario=salario+aumento;
+    printf("valor do aumento:%.2f\n",aumento);
+    printf("valor do novo salario:%.2f\n",novoSalario);
+    system("pause");
 
     return 0;
 }
